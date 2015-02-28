5c886e9b38cc937e34a22e825b05d9346f74d5e0
Samuel Lipschutz <lipschut@walleye.nscl.msu.edu>
2015-02-28 12:24:34 -0500

    Just different settings

--- src/Settings.cc
+++ src/Settings.cc
@@ -4,9 +4,9 @@
 
 namespace Settings{
 
-  int NumberOfPeople=500;
+  int NumberOfPeople=50;
   int NumberOfInteractionsPerStep=1;
-  int NumberOfSteps=50;
+  int NumberOfSteps=500000;
   int MaxGoodNumber=10;
   double MaxGoodPriority=100.;
 }
