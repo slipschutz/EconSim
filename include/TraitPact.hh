6baedaca451bb9752c9f9fe42c46235c4f961540
sam <lipschutz@nscl.msu.edu>
2016-10-22 18:32:20 -0400

    Added some tempalates that i probably won't use to the the trait pact class

--- include/TraitPact.hh
+++ include/TraitPact.hh
@@ -1,16 +1,32 @@
 #ifndef __TRAITPACT_HH
 #define __TRAITPACT_HH
 
-
-
-
-
-
+#include <vector>
+#include <boost/any.hpp>
 
 /**
    Abstract parent class for containers to hold traits for the actors
    will be extened for each class of Economic Actor
  */
+
+
+// class Trait{
+// public:
+
+//   virtual void * Get()=0;
+
+
+// };
+
+
+// template <typename T>
+// class TraitImpl : public Trait{
+
+// private:
+//   T val;
+
+// };
+
 
 
 
@@ -22,22 +38,27 @@ public:
   virtual ~TraitPact();
   virtual void InitializeTraits()=0;
 
-  void BoundAboveZero(int &val);
-  void BoundAboveZero(float&val);
-  template<typename T> void BoundAboveZero(T & val);
-  
-  void BoundZeroToOne(float &val);
-    
 
-protected:
 
-  
+  template <typename T> void AddTrait(T val){
+    theTraits.push_back(val);
+  }
+
+  template <typename T> void GetTrait(int index,T & val){
+    val=boost::any_cast<T>(theTraits.at(index));
+  }
+
+  boost::any GetTrait(int index){
+    return theTraits.at(index);
+  }
+
+
+
+  int GetSize(){return theTraits.size();}
+
+private:
+  //a vector of boost any objects will hold the traits
+  std::vector <boost::any> theTraits;  
 };
-
-
-template<typename T> void TraitPact::BoundAboveZero(T & val){
-
-}
-
 
 #endif /* __TRAITPACT_HH */
--- src/TraitPact.cc
+++ src/TraitPact.cc
@@ -5,29 +5,8 @@
 #include "TraitPact.hh"
 
 
-void TraitPact::BoundAboveZero(int &val){
-  if (val <0){
-    val=0;
-  }
-}
 
-void TraitPact::BoundAboveZero(float & val){
-  if (val <0){
-    val=0;
-  }
-}
-
-
-void TraitPact::BoundZeroToOne(float &val){
-  if (val <0){
-    val=0;
-  }else if (val>1){
-    val=1;
-  }
-}
 TraitPact::TraitPact(){
-  double x=5;
-  BoundAboveZero(x);
   
 }
 TraitPact::~TraitPact(){
