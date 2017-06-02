219b01ecc620b6c664f1b897f009008792a419c3
Sam Lipschutz <samlipschutz@Sams-MacBook-Pro.local>
2017-06-02 10:06:31 -0600

    This should have been in last commit.  There interface class has had additional virtual function added so that they can be used in the implementation class

--- include/NeuralNetworkInterface.hh
+++ include/NeuralNetworkInterface.hh
@@ -6,6 +6,8 @@
 #include <functional>
 #include <vector>
 
+#include "Eigen/Dense"
+
 /**ABC to serve as interface for neural network implementation
    of economic actor decision making
  */
@@ -17,9 +19,11 @@ public:
   
   virtual void AddInput(std::string,double *)=0;
   virtual void AddAction(std::string,std::function<void()>)=0;
-  virtual void Think()=0;
+  virtual void SetInputReferences(std::vector<double> &)=0;
+  virtual Eigen::VectorXd Think()=0;
   virtual void Print()=0;
   virtual void Train()=0;
+  virtual NeuralNetworkInterface* MutateCopy(double)=0;
   
 };
 
